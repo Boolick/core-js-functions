a53d9c6e01b655cb2369f48519aab5b5639b3e9c
Boolick <boolick38@gmail.com>
2024-04-15 23:00:13 +0300

    fix: fix partialUsingArguments, getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -220,8 +220,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    const result = fn(...args1, ...args2);
+    return result;
+  };
 }
 
 /**
@@ -241,8 +244,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const currentId = id;
+    id += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
